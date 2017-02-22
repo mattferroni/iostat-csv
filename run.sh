3ff66966355e95e0a2651dfe70b6c77f73a2ba15
Matteo Ferroni <matteo@bottega52.it>
2017-02-22 12:10:22 +0100

    Minor changes

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-iostat -t -x 1 | grep --line-buffered -v -e avg-cpu -e Device -e Linux >> /tmp/iostat.log
+iostat -t -x 1 | grep --line-buffered -v -e avg-cpu -e Device -e Linux >> /tmp/iostat.log &
 
 while true; do
 	ps -C java -o pid=,%mem=,vsz= >> /tmp/mem.log
-	gnuplot /tmp/gnuplot.script
+	gnuplot ./gnuplot.script
 	sleep 1
 done
